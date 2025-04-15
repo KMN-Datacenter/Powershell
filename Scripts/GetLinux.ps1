0b8b15bc047947f47a376756b4329699f49430bf
John ORaw <john.oraw@kmn.ie>
2025-04-15 21:05:50 +0100

    Added GetLinux

---
+++ Scripts/GetLinux.ps1
@@ -0,0 +1,11 @@
+﻿<#
+Script: GetLinux.ps1
+Author: JOR
+Purpose: Extract all users with linux properties
+Date Created: 21/12/24
+#>
+
+Import-Module ActiveDirectory
+$ExportPath="D:\Github\Users\WindowsExtracts\user_dump.csv"
+Get-ADUser -Filter * -Properties * | select givenName,sn,SamAccountName,unixHomeDirectory,loginShell,uidNumber,gidnumber | Export-Csv -NoType $ExportPath
+  
