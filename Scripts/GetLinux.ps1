7152ff8f4c0b3640ff3dbed76c1d978370ee4468
Harman Singh <harman.singh@atu.ie>
2026-05-22 16:14:52 +0100

    Revise GetLinux.ps1 script for user extraction
    Updated script metadata and modified user extraction logic.

--- Scripts/GetLinux.ps1
+++ Scripts/GetLinux.ps1
@@ -1,11 +1,22 @@
-﻿<#
-Script: GetLinux.ps1
-Author: JOR
-Purpose: Extract all users with linux properties
-Date Created: 21/12/24
+<#
+.SYNOPSIS
+  <Get Linux info from AD>
+.DESCRIPTION
+  <Simple script to list existing AD users and their UID, GID, home directory>
+.OUTPUTS
+  <List of users sAMAccountName, UID , GID, home directory>.log>
+.NOTES
+  Version:        1.0
+  Author:         <JOR>
+  Creation Date:  <20250825>
+  Purpose/Change: Initial script  
 #>
 
-Import-Module ActiveDirectory
-$ExportPath="D:\Github\Users\WindowsExtracts\user_dump.csv"
-Get-ADUser -Filter * -Properties * | select givenName,sn,SamAccountName,unixHomeDirectory,loginShell,uidNumber,gidnumber | Export-Csv -NoType $ExportPath
-  
+
+# Enable-PSRemoting -Force
+# Enter account details for an elevated user
+$Cred = Get-Credential
+# Login to dc1
+Enter-PSSession -ComputerName dc1 -Credential $Cred
+
+Get-ADUser -Filter * -Properties * | select SamAccountName,uidNumber, gidnumber, unixHomeDirectory
